d8793e1fc1e633c75ad296714f13ec3d80712dac
Darcness <darcness@gmail.com>
2024-03-23 14:15:46 -0400

    Small manifest update

--- gulp/manifest.ts
+++ gulp/manifest.ts
@@ -30,7 +30,7 @@ function getManifest() {
     manifest.title = manifest?.title ?? name;
     manifest.version = manifest?.version ?? version;
     manifest.description = manifest?.description ?? description;
-    manifest.manifest = manifest?.manifest ?? `https://raw.githubusercontent.com/Darcness/hlo-import-foundryvtt/blob/v${version}/module.json`;
+    manifest.manifest = manifest?.manifest ?? `https://github.com/Darcness/hlo-import-foundryvtt/releases/download/v${version}/module.json`;
     manifest.download = manifest?.download ?? `https://github.com/Darcness/hlo-import-foundryvtt/releases/download/v${version}/${name}-v${version}.zip`;
     return manifest;
 }
